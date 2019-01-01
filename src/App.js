5ddf5d5f980c8e867f7ff33d854b6d98168a3ea5
Taras <taras.prezlyata.sign.in@gmail.com>
2019-01-01 21:27:31 +0200

    add style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Image, Grid, Row, Col, Jumbotron, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 
 class App extends Component {
 	constructor() {
@@ -42,29 +43,65 @@ class App extends Component {
 		console.log(this.state);
 		const persons = this.state.persons.map((item, i) => (
 			<div key={i}>
-				{this.state.namePeople.length > 0 && <h1>Name: {this.state.namePeople}</h1>}
-				{this.state.namePeople.length === 0 && <h1>Name: {item.name.first}</h1>}
-				<img src={item.picture.large} alt="" />
-				<h4> City: {item.location.city}</h4>
+				{this.state.namePeople.length > 0 && (
+					<h2>
+						Name:
+						{' ' +
+							this.state.namePeople.charAt(0).toUpperCase() +
+							this.state.namePeople.substring(1, this.state.namePeople.length)}
+					</h2>
+				)}
+				{this.state.namePeople.length === 0 && (
+					<h2>
+						Name:
+						{' ' +
+							item.name.first.charAt(0).toUpperCase() +
+							item.name.first.substring(1, item.name.first.length)}
+					</h2>
+				)}
+				<Grid>
+					<Row>
+						<Col xs={6} md={4}>
+							<Image src={item.picture.large} circle />
+						</Col>
+					</Row>
+				</Grid>
+				<h4>Age: {item.dob.age}</h4>
+				<h4>
+					City:
+					{' ' +
+						item.location.city.charAt(0).toUpperCase() +
+						item.location.city.substring(1, item.location.city.length)}
+				</h4>
 				<span>
-					Phone: {item.cell}, Email: {item.email}
+					<h5>
+						Phone: {item.cell}; Email: {item.email}
+					</h5>
 				</span>
 			</div>
 		));
 		return (
 			<div className="container">
-				{this.state.loading === false && (
-					<div>
-						<h1>My name is {this.state.autor}</h1>
-						<div>{persons}</div>
-						<form>
-							<label>
-								Chage Name:
-								<input onChange={this.changeName.bind(this)} type="text" name="name" />
-							</label>
-						</form>
-					</div>
-				)}
+				<Jumbotron>
+					{this.state.loading === false && (
+						<div>
+							<h1>Random users {this.state.autor}</h1>
+							<div>{persons}</div>
+							<form>
+								<FormGroup>
+									<ControlLabel>Change Name</ControlLabel>
+									<FormControl
+										onChange={this.changeName.bind(this)}
+										type="text"
+										name="name"
+										placeholder="Enter name"
+									/>
+									<FormControl.Feedback />
+								</FormGroup>
+							</form>
+						</div>
+					)}
+				</Jumbotron>
 			</div>
 		);
 	}
